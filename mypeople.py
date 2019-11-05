4d2792f3482142efc210ff7a9592fd52e6088e40
Esuabom Dijemeni <e.dijemeni@gmail.com>
2019-11-05 13:42:58 +0000

    2019/11/05 16 - 100% - completed Display Person Window

--- mypeople.py
+++ mypeople.py
@@ -51,11 +51,10 @@ class MyPeople(Toplevel):
         btnadd = Button(self.bottomFrame, text='Add', width=12, font='Sans 12 bold', command=self.funcaddPeople)
         btnadd.grid(row=0, column=2, sticky=N, padx=10, pady=10)
 
-        btnupdate = Button(self.bottomFrame, text='Update', width=12, font='Sans 12 bold',
-                            command=self.funcUpdatePerson)
+        btnupdate = Button(self.bottomFrame, text='Update', width=12, font='Sans 12 bold', command=self.funcUpdatePerson)
         btnupdate.grid(row=0, column=2, sticky=N, padx=10, pady=50)
 
-        btndisplay = Button(self.bottomFrame, text='Display', width=12, font='Sans 12 bold')
+        btndisplay = Button(self.bottomFrame, text='Display', width=12, font='Sans 12 bold', command=self.funcDisplayPerson)
         btndisplay.grid(row=0, column=2, sticky=N, padx=10, pady=90)
 
         btndelete = Button(self.bottomFrame, text='Delete', width=12, font='Sans 12 bold')
@@ -71,6 +70,14 @@ class MyPeople(Toplevel):
         person = self.listBox.get(selected_item)
         person_id = person.split("-")[0]
         updatepage = Update()
+
+    def funcDisplayPerson(self):
+        global person_id
+        selected_item = self.listBox.curselection()
+        person = self.listBox.get(selected_item)
+        person_id = person.split("-")[0]
+        displaypage = Display()
+
 
 class Update(Toplevel):
     def __init__(self):
@@ -168,3 +175,83 @@ class Update(Toplevel):
             self.destroy()
         except:
             messagebox.showinfo("Success", "Person has not been updated", icon='warning')
+
+class Display(Toplevel):
+    def __init__(self):
+        Toplevel.__init__(self)
+        self.geometry("650x750+550+200")
+        self.title("Display Person")
+        self.resizable(False, False)
+
+        # get person from database
+        global person_id
+
+        person=cur.execute("SELECT * FROM persons WHERE person_id = ?",(person_id,))
+        person_info = person.fetchall()
+        self.person_id = person_info[0][0]
+        self.person_name = person_info[0][1]
+        self.person_surname = person_info[0][2]
+        self.person_email = person_info[0][3]
+        self.person_phone = person_info[0][4]
+        self.person_address = person_info[0][5]
+        # print(self.person_id, '\n',
+        #         self.person_name, '\n',
+        #         self.person_surname, '\n',
+        #         self.person_email, '\n',
+        #         self.person_phone, '\n',
+        #         self.person_address)
+
+        # Frames
+        self.top = Frame(self,height=150, bg='white')
+        self.top.pack(fill=X)
+        self.bottomFrame = Frame(self, height=600, bg='#fcc324')
+        self.bottomFrame.pack(fill=X)
+
+        # Heading, image and data
+        self.top_image = PhotoImage(file='./icons/addperson.png')
+        self.top_image_lbl = Label(self.top, image=self.top_image, bg='white')
+        self.top_image_lbl.place(x=120, y = 10)
+        self.heading = Label(self.top, text='Add Person', font='arial 15 bold',
+                             fg='#003f8a', bg='white')
+        self.heading.place(x=260, y=60)
+
+        ##########################################################################
+        # First name
+        self.lbl_name = Label(self.bottomFrame, text=' First name', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_name.place(x=40, y=40)
+        self.ent_name =  Entry(self.bottomFrame, width=30, bd=4)
+        self.ent_name.insert(0, self.person_name)
+        self.ent_name.config(state='disabled')
+        self.ent_name.place(x=150, y=45)
+
+        # Last name
+        self.lbl_surname = Label(self.bottomFrame, text='Surname', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_surname.place(x=40, y=80)
+        self.ent_surname =  Entry(self.bottomFrame, width=30, bd=4)
+        self.ent_surname.insert(0, self.person_surname)
+        self.ent_surname.config(state='disabled')
+        self.ent_surname.place(x=150, y=85)
+
+        # Email
+        self.lbl_email = Label(self.bottomFrame, text='Email', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_email.place(x=40, y=120)
+        self.ent_email =  Entry(self.bottomFrame, width=30, bd=4)
+        self.ent_email.insert(0, self.person_email)
+        self.ent_email.config(state='disabled')
+        self.ent_email.place(x=150, y=125)
+
+        # Phone Number
+        self.lbl_phone = Label(self.bottomFrame, text='Phone', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_phone.place(x=40, y=160)
+        self.ent_phone =  Entry(self.bottomFrame, width=30, bd=4)
+        self.ent_phone.insert(0, self.person_phone)
+        self.ent_phone.config(state='disable')
+        self.ent_phone.place(x=150, y=165)
+
+        # Address
+        self.lbl_address = Label(self.bottomFrame, text='Address', font='arial 15 bold', fg='white', bg='#fcc324')
+        self.lbl_address.place(x=40, y=310)
+        self.address =  Text(self.bottomFrame, width=40, height=15, wrap=WORD)
+        self.address.insert('1.0', self.person_address)
+        self.address.config(state='disable')
+        self.address.place(x=150, y=210)
